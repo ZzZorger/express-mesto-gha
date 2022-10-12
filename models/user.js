aeb6b3a3e6ee5eb50ff0c1e54ced3da484049348
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-10-12 23:43:20 +0300

    Начало пр14

--- models/user.js
+++ models/user.js
@@ -5,18 +5,36 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 8,
     required: true,
   },
 });
+// userSchema.static.validateEmail = function (email) {
+//   return this.findOne({ email })
+//   .then((user) {
+//     if (!user) {
+//       return Promise.reject.compare(new Error('Неправильные почта или пароль'));
+//     }
+//   })
+// };
 
 module.exports = mongoose.model('user', userSchema);
