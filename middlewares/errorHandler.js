1ad9d7c1a54eb23bf3d001d3aa3afa772d858de2
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-10-26 01:29:44 +0300

    Проверка автотестов

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,8 +2,8 @@ const { isCelebrateError } = require('celebrate');
 
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message = 'Неизвестная ошибка сервера' } = err;
-  if (isCelebrateError(err)) {
-    res.status(statusCode).json(err);
+  if (!isCelebrateError(err)) {
+    res.status(err.errorCode).send({ message: err.message });
   } else {
     res.status(statusCode)
       .json({ message: statusCode === 500 ? 'Неизвестная ошибка сервера' : message });
