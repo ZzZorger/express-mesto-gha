e0116754cdeb1fbde711d5d2a168af782e554cf6
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-10-26 01:32:10 +0300

    Проверка автотестов

--- errors/UnauthorizedError.js
+++ errors/UnauthorizedError.js
@@ -2,7 +2,7 @@ class UnauthorizedError extends Error {
   constructor(message) {
     super(message);
     this.name = 'unauthorizedError';
-    this.errorCode = 400;
+    this.errorCode = 401;
   }
 }
 
