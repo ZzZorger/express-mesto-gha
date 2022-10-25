c7d9d3fb6262b89e1028c7549e0da33aeaac582d
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-10-25 23:35:37 +0300

    Проверка автотестов

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(URLregex),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
