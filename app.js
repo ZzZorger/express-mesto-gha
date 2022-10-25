e9aa89a6f62d54ae975f4aab918695f4c936b39d
Alexandr Yakushkin <yalex007@mail.ru>
2022-10-25 13:59:04 +0300

    Добавление обработчика joi

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cardRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
+const { celebrate, Joi } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -22,11 +23,40 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  })
+}) ,login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  })
+}), createUser);
 app.use(auth);
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
+app.use('/users', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  })
+}), userRouter);
+app.use('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string(),
+    owner: Joi.string().hex().length(24).required(),
+    likes: Joi.string().hex().length(24).required(),
+    createdAt: Joi.date(),
+  })
+}), cardRouter);
 app.use('*', (req, res) => (
   res.status(404).send({ message: 'Страница не найдена' })
 ));
