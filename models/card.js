19985f4e9ce054eb31ea2a69ea4363d9ae9615df
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-10-29 19:02:16 +0300

    Доработки ревью 1

--- models/card.js
+++ models/card.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const linkRegexp = /http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+\.(?:jpg|gif|png|bmp)/;
+const linkRegexp = /http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+\./;
 
 const cardSchema = new mongoose.Schema({
   name: {
